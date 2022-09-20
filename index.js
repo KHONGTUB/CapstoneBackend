a8acad0aa362321eb2fc5e16aaf3f720db6f58d4
KHONGTUB <jkojeda12@gmail.com>
2022-09-20 00:12:24 -0500

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/data");
+const cors = require("cors");
 
 const PORT = 5001;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hey welcome to my server");
